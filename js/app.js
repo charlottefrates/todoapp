21fe1b10f50d636455108a4eaf72a20de01caba4
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-03-01 10:54:56 -0800

    Planning stage

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// Problem: User interaction doesnt provide desired results
+// Solution: Add interactivity so that use can manage daily tasks
+
+//Add a new
+     //when the button is pressed
+     // Create a a new list item with the text from #new-task:
+          // input (checkbox)
+          //label
+          // input (text)
+          // button.edit
+          //button.delete
+          //Each element, needs modified and appended
+
+
+//Edit Existing task
+     //When the edit button is pressed
+          // if the class of the parent is .editMode
+               //Switch from .editMode
+               //label text become input's value
+          // else
+               //switch to .editMode
+               //input value becomes the label's text
+          //toggle .editmode on the parent
+
+//Delete an exisiting task
+     //When the delete button is pressed
+          //remove the parent list item from the ul
+
+
+//Mark a task as complete
+     //When the checkbox is checked
+          //Append the task list item to the #completed-tasks
+
+
+//Mark a task as incomplete
+//When the checkbox is unchecked
+     //Append the task list item to the #incompleted-task
